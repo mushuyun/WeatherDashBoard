64d30a8531e31a575f6f5dcf6134d16948955062
mushuyun <mushuyun@yahoo.com>
2020-02-09 17:02:27 -0500

    update files

--- script-jquery.js
+++ script-jquery.js
@@ -1,32 +1,73 @@
-//need to show cities, uvindex;
 
   var url;
   var city;
   var key;
+  var lat;
+  var lon;
 
-  //get today's date
-  $("#date").html(new Date());
+//get today's date
+$("#date").html(new Date());
+
+//hardcode data showing default weather info
+$.ajax({
+  url: 'http://api.openweathermap.org/data/2.5/weather?q=Morrisville&APPID=80dbdb1e6f8e0d8a44b9a417a96e7403',
+  method: "GET"
+})
   
-  
-  $("#search-btn").on("click", function(){
-      
-      event.preventDefault();
-      var city = $("#city-search").val();
-      if (city === "") {
-        return;
-        } else  { 
-            getWeather();
+    .then(function(res){
+
+          var weatherDetail = {
+          city: res.name,
+          temperature: Math.floor(res.main.temp - 273),
+          humidity: res.main.humidity,
+          windSpeed: res.wind.speed,
+          lon: res.coord.lon,
+          lat: res.coord.lat
+          };
+          
+          var iconcode = res.weather[0].icon;
+          var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+          $("#wicon").attr('src', iconurl);
+         
+          var result1 = $("<h6>").text("City: " + weatherDetail.city);
+          var result2 = $("<h6>").text("Temperature: " + weatherDetail.temperature + " °C");
+          var result3 = $("<h6>").text("Humidity: " + weatherDetail.humidity + "%");
+          var result4 = $("<h6>").text("Wind Speed: " + weatherDetail.windSpeed);
             
-            localStorage.setItem("city", JSON.stringify(city));
-            }
+          $("#weather-detail").append(result1, result2, result3, result4); 
+          $.ajax({
+            url: "https://api.openweathermap.org/data/2.5/uvi?APPID=80dbdb1e6f8e0d8a44b9a417a96e7403&lat=35.82&lon=-78.82",
 
-           console.log(localStorage.getItem("city"))
+              method: "GET"
+          })
+             .then(function(data){
+               var uvIndex = data.value;
+               var result5 = $("<h6>").text("UV Index: " + uvIndex);
+               $("#weather-detail").append(result5);
+             });
+      });
+
+ // loading wether by enter city
+$("#search-btn").on("click", function(){
     
-    var pastCities =  $("<p>").text(JSON.parse(localStorage.getItem("city")));
-    $("#past-cities").append(pastCities);
-            
-  });
-   
+    event.preventDefault();
+    
+    var city = $("#city-search").val();
+    if (city === "") {
+      return;
+      } else  { 
+          getWeather();
+          
+          localStorage.setItem("city", JSON.stringify(city));
+          }
+
+          console.log(localStorage.getItem("city"))
+  
+  var pastCities =  $("<p>").text(JSON.parse(localStorage.getItem("city")));
+  $("#past-cities").append(pastCities);
+  localStorage.setItem("city", JSON.stringify(pastCities))
+          
+});
 
 //get weather information
   function getWeather(){
@@ -57,13 +98,12 @@
                 var result1 = $("<h6>").text("City: " + weatherDetail.city);
                 var result2 = $("<h6>").text("Temperature: " + weatherDetail.temperature + " °C");
                 var result3 = $("<h6>").text("Humidity: " + weatherDetail.humidity + "%");
-                var result4 = $("<h6>").text("Wind: " + weatherDetail.windSpeed);
+                var result4 = $("<h6>").text("Wind Speed: " + weatherDetail.windSpeed);
                 
                 $("#weather-detail").empty();
-                
-                 
+                  
                 $("#weather-detail").append(result1, result2, result3, result4); 
-
+        
          //use latitude and logitude got from above to make uvIndex call
                 var lon = res.coord.lon;
                 var lat = res.coord.lat;
@@ -76,7 +116,7 @@
                      var uvIndex = data.value;
                      var result5 = $("<h6>").text("UV Index: " + uvIndex);
                      $("#weather-detail").append(result5);
-                   })
+                   });
           //use latitude and logitude got from above to make 5day forcast call
                    
                   $.ajax({
@@ -84,6 +124,7 @@
                     url: "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&APPID=80dbdb1e6f8e0d8a44b9a417a96e7403",
                     method: "GET"
                   })
+                  
                   .then (function(forcast){
                     for(var i = 0; i < 5; i++){
                       var temp = Math.floor((forcast.list[i].main.temp - 273));
@@ -91,22 +132,32 @@
                       var humi = forcast.list[i].main.humidity;
                       var icon = forcast.list[i].weather[0].icon;
                       
-                        var iconLink ='http://openweathermap.org/img/w/'+ icon + ".png";
-                        $("#wicon" + i).attr("src", iconLink);
-                        
-                        var res1 =  $("<p>").text("Tempreture: " + temp +" °C");
-                        var res2 = $("<p>").text("Humidity: "+ humi + "%");
+                      var iconLink ='http://openweathermap.org/img/w/'+ icon + ".png";
+                      $("#wicon" + i).attr("src", iconLink);
+                      
+                      var res1 =  $("<p>").text("Tempreture: " + temp +" °C");
+                      var res2 = $("<p>").text("Humidity: "+ humi + "%");
 
-                        
-
-                        $("#temp" + i).append(res1);
-                        $("#humi" + i).append(res2);
-          
+                      $("#temp" + i).empty();
+                      $("#humi" + i).empty();
+                      $("#temp" + i).append(res1);
+                      $("#humi" + i).append(res2);
+                      
                     }    
                             
-                  })    
-         
+                  });           
           });    
     }
   }
+
+//set date on forcast days
+
+var day = moment().date();
+var mon = moment().month();  // jan=0, dec=11
+var year = moment().year();
+
+for (i=0; i<5; i++){
+  var dayFuture = $("<h6>").text(parseInt(mon+1) + "/" + parseInt(day+1+i));
+  $("#day" + i).prepend(dayFuture);
+}
 
